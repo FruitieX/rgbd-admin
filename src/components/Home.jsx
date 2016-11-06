e6ecac2602b341d9f93aea5af7266cb214c94451
Rasmus Eskola <fruitiex@gmail.com>
2016-11-06 18:35:52 +0200

    Show LED strip configuration, support multiple strips

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import io from 'socket.io-client';
 
 const canvasStyle = {
-  'background-color': 'black',
-  'width':            '100%',
+  'backgroundColor': 'black',
+  'width':           '100%',
 };
 
 export default class Home extends React.Component {
@@ -11,21 +11,43 @@ export default class Home extends React.Component {
     super();
 
     this.state = {
-      socket: io.connect('http://localhost:9009'),
+      socket: io.connect('http://192.168.1.101:9009'),
+      strips: [],
     };
+  }
+
+  componentDidMount() {
     this.state.socket.emit('stripsSubscribe');
+    this.state.socket.on('strips', strips => {
+      strips.forEach((strip, index) => {
+        this.updateCanvas(strip, index);
+      });
 
-    this.state.socket.on('strips', data => {
-      const canvas = document.getElementById('strip');
-      const ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      this.setState({
+        strips,
+      });
+    });
+  }
 
-      const colors = data[0].colors;
+  componentWillUnmount() {
+    this.state.socket.disconnect();
+  }
 
-      colors.forEach((color, index) => {
+  updateCanvas() {
+    const canvas = this.refs.strip0;
+
+    if (!canvas) {
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    this.state.strips.forEach(strip => {
+      strip.colors.forEach((color, index) => {
         const h = canvas.height;
-        const w = canvas.width / colors.length;
-        const offset = index * canvas.width / colors.length;
+        const w = canvas.width / strip.colors.length;
+        const offset = index * canvas.width / strip.colors.length;
 
         ctx.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
         ctx.beginPath();
@@ -35,14 +57,31 @@ export default class Home extends React.Component {
     });
   }
 
-  componentWillUnmount() {
-    this.state.socket.disconnect();
-  }
-
   render() {
+    const strips = [];
+
+    this.state.strips.forEach((strip, index) => {
+      strips.push(
+        <div key={index}>
+          <div>
+            <b>RGB LED strip {index}, "{strip.name}"</b>
+            <div>
+              Configuration: {strip.dev}, {strip.numLeds} LEDs, reversed: {strip.reversed ? 'true' : 'false'}
+            </div>
+          </div>
+
+          <canvas style={ canvasStyle } width={1920} ref={`strip${index}`} />
+
+          <div>
+            Avg framerate: {Math.round(strip.fps)}
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div>
-        <canvas style={canvasStyle} width={1920} id='strip'/>
+        { strips }
       </div>
     );
   }
